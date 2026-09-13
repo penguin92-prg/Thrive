b7084f111c06ca940ec185e25910489c1ce3e95a
penguin92-prg <penguin92.prg@gmail.com>
2026-09-13 09:26:37 +0900

    Update キッカーの動作確認プログラムを修正
    通常用ソレノイドとチップキック用ソレノイドの両方に通電させるように変更

--- program/offender/dev/teensy-kicker/teensy-kicker.ino
+++ program/offender/dev/teensy-kicker/teensy-kicker.ino
@@ -1,13 +1,20 @@
+constexpr KICKER_STROKE_PIN = 30;
+constexpr KICKER_CHIP_PIN = 31;
+
 void setup() {
-  pinMode(31, OUTPUT);
+  pinMode(KICKER_STROKE_PIN, OUTPUT);
+  pinMode(KICKER_CHIP_PIN, OUTPUT);
 
   Serial.begin(115200);
 }
 
 void loop() {
-  digitalWrite(31, HIGH);
-  Serial.println("Kicked!");
-  delay(5000);
-  digitalWrite(31, LOW);
+  digitalWrite(KICKER_STROKE_PIN, HIGH);
+  digitalWrite(KICKER_CHIP_PIN, HIGH);
+  Serial.println("Kick Start!");
+  delay(50);
+  digitalWrite(KICKER_STROKE_PIN, LOW);
+  digitalWrite(KICKER_CHIP_PIN, LOW);
+  Serial.println("Kick Stop!");
   delay(5000);
 }
