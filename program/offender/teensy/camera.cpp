30521126977d35ec6522cd504e17835949e2d556
penguin92-prg <penguin92.prg@gmail.com>
2026-09-18 14:50:24 +0900

    Update ボール追従プログラムで作成したcamera.hpp/camera.cppの内容を反映

--- program/offender/teensy/camera.cpp
+++ program/offender/teensy/camera.cpp
@@ -34,7 +34,7 @@ void Camera::receive(HardwareSerialIMXRT& camSerial){
   x[1] = camSerial.read();
   y = camSerial.read();
 
-  ball.x = (x[0] << 8) | x[1];
+  ball.x = ((x[0] << 8) | x[1]) == 512 ? -1 : ((x[0] << 8) | x[1]);
 
   ball.y = y == 255 ? -1 : y;
 
@@ -45,5 +45,47 @@ void Camera::receive(HardwareSerialIMXRT& camSerial){
 }
 
 void Camera::send(HardwareSerialIMXRT& serial){
+  // カメラへのデータ送信
+}
+
+float Camera::calcDeg(){
+  if(ball.x == -1 || ball.y == -1){
+    return -1.0f;
+  }
   
+  // カメラ設置角度を算出
+  cameraDeg.y = 90*cameraNum;
+  
+  // カメラ設置座標を算出
+  float cameraRadY = cameraDeg.y / 180.0f * 3.14159265;
+  cameraPos.x = 0.05*sin(cameraRadY);
+  cameraPos.y = 0.11;
+  cameraPos.z = 0.05*cos(cameraRadY);
+  
+  // ボール位置をカメラからの相対座標に変換
+  ball3.x = ball.x - cameraCenter.x;
+  ball3.y = ball.y - cameraCenter.y;
+  ball3.z = cameraFocus.x;
+
+  // ベクトルの大きさを1にして単位ベクトルに変換
+  float norm = ball3.len();
+  ball3 /= norm;
+  // if(norm > 0.0001f){
+  //   ball3 /= norm;
+  // }
+
+  // 回転行列を計算
+  d1 = Matrix33::rotZ(cameraDeg.z) * ball3;
+  d2 = Matrix33::rotX(cameraDeg.x) * d1;
+  d = Matrix33::rotY(cameraDeg.y) * d2;
+
+  // コート床との交点算出
+  float t = cameraPos.y / d.y;
+
+  コート上での2次元座標の算出
+  court.x = cameraPos.x + d.x * t;
+  court.y = cameraPos.z + d.z * t;
+
+  // ボールの角度を算出
+  return atan2(ball.y, ball.x) * 180.0f / 3.14159265;
 }
--- program/offender/teensy/camera.hpp
+++ program/offender/teensy/camera.hpp
@@ -2,14 +2,41 @@
 
 #include <Arduino.h>
 
+#include "matrix33.hpp"
 #include "vec2.hpp"
 
 class Camera{
   private:
+    // 受信データのデータ長（byte）
     static constexpr uint8_t STR_SIZE = 3;
 
+    // カメラ番号
+    uint8_t cameraNum;
+
+    // カメラの焦点距離
+    static constexpr Vec2 cameraFocus = {230.0f, 240.0f};
+
+    // カメラの中心座標
+    static constexpr Vec2 cameraCenter = {160.0f, 120.0f};
+
+    // カメラの設置座標
+    Vec3 cameraPos;
+
+    // カメラの設置角度（deg）
+    Vec3 cameraDeg = {-25.0f, 0.0f, 0.0f};
+
+    // カメラからボールの向きの3次元単位ベクトル
+    Vec3 ball3;
+
+    Vec3 d1;
+    Vec3 d2;
+    Vec3 d;
+
+    // コート上でのボールの2次元座標
+    Vec2 court = {0.0f, 0.0f};
+
   public:
-    Camera() = default;
+    explicit Camera(uint8_t num = 0) : cameraNum(num);
     ~Camera() = default;
 
     Vec2 ball;
@@ -17,6 +44,8 @@ class Camera{
     void init(HardwareSerialIMXRT&, int);
     void receive(HardwareSerialIMXRT&);
     void send(HardwareSerialIMXRT&);
+
+    float calcDeg();
 };
 
 extern Camera camera1;
