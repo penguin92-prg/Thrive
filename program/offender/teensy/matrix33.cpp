ab17e36028927ff3d4a5b715c24055f98cb8e370
penguin92-prg <penguin92.prg@gmail.com>
2026-09-18 14:50:43 +0900

    Create カメラデータの計算に必要な3次元ベクトルと3x3正方行列の構造体を作成

---
+++ program/offender/teensy/matrix33.cpp
@@ -0,0 +1,119 @@
+#include <math.h>
+
+#include "matrix33.hpp"
+
+Vec3 operator+(const Vec3& v1, const Vec3& v2){
+  Vec3 v;
+  v.x = v1.x + v2.x;
+  v.y = v1.y + v2.y;
+  v.z = v1.z + v2.z;
+  return v;
+}
+
+Vec3 operator-(const Vec3& v1, const Vec3& v2){
+  Vec3 v;
+  v.x = v1.x - v2.x;
+  v.y = v1.y - v2.y;
+  v.z = v1.z - v2.z;
+  return v;
+}
+
+Vec3 operator*(const Vec3& v, const float f){
+  Vec3 r;
+  r.x = v.x * f;
+  r.y = v.y * f;
+  r.z = v.z * f;
+  return r;
+}
+
+Vec3 operator*(const float f, const Vec3& v){
+  Vec3 r;
+  r.x = f * v.x;
+  r.y = f * v.y;
+  r.z = f * v.z;
+  return r;
+}
+
+Vec3& Vec3::operator+=(const Vec3& v){
+  x += v.x;
+  y += v.y;
+  z += v.z;
+  return *this;
+}
+
+Vec3& Vec3::operator-=(const Vec3& v){
+  x -= v.x;
+  y -= v.y;
+  z -= v.z;
+  return *this;
+}
+
+Vec3& Vec3::operator*=(const float f){
+  x *= f;
+  y *= f;
+  z *= f;
+  return *this;
+}
+
+Vec3& Vec3::operator/=(const float f){
+  x /= f;
+  y /= f;
+  z /= f;
+  return *this;
+}
+
+Vec3& Vec3::operator=(const Vec3& v){
+  x = v.x;
+  y = v.y;
+  z = v.z;
+  return *this;
+}
+
+
+void Vec3::set(float vec_x, float vec_y, float vec_z){
+  x = vec_x;
+  y = vec_y;
+  z = vec_z;
+  return;
+}
+
+void Vec3::clear(){
+  x = 0.0f;
+  y = 0.0f;
+  z = 0.0f;
+  return;
+}
+
+void Vec3::len(){
+  return math.sqrt(x**2 + y**2 + z**2);
+}
+
+Matrix33 operator+(const Matrix33& m1, const Matrix33& m2){
+  return Matrix33(m1.v1+m2.v1, m1.v2+m2.v2, m1.v3+m2.v3);
+}
+
+Matrix33 operator-(const Matrix33& m1, const Matrix33& m2) {
+  return Matrix33(m1.v1-m2.v1, m1.v2-m2.v2, m1.v3-m2.v3);
+}
+
+Vec3 operator*(const Matrix33& m, const float f){
+  return Matrix33(m.v1*f, m.v2*f, m.v3*f);
+}
+
+Vec3 operator*(const Matrix33& m, const Vec3& v) {
+  return Vec3(
+    m.v1.x * v.x + m.v1.y * v.y + m.v1.z * v.z,
+    m.v2.x * v.x + m.v2.y * v.y + m.v2.z * v.z,
+    m.v3.x * v.x + m.v3.y * v.y + m.v3.z * v.z
+  );
+}
+
+Matrix33 operator*(const Matrix33& m1, const Matrix33& m2) {
+  Matrix33 result;
+  for (int r = 0; r < 3; ++r) {
+    for (int c = 0; c < 3; ++c) {
+      result[r][c] = m1[r][0]*m2[0][c] + m1[r][1]*m2[1][c] + m1[r][2]*m2[2][c];
+    }
+  }
+  return result;
+}
---
+++ program/offender/teensy/matrix33.hpp
@@ -0,0 +1,102 @@
+#pragma once
+
+#include <Arduino.h>
+#include <math.h>
+
+class Vec3{
+  public:
+    float x, y, z;
+    Vec3() : x(0.0f), y(0.0f), z(0.0f){}
+    Vec3(float vec_x, float vec_y, float vec_z) : x(vec_x), y(vec_y), z(vec_z){}
+
+    float& operator[](int i){
+      return (i==0) ? x : (i==1) ? y : z;
+    }
+    const float& operator[](int i) const{
+      return (i==0) ? x : (i==1) ? y : z;
+    }
+
+    Vec3& operator+=(const Vec3& v);
+    Vec3& operator-=(const Vec3& v);
+    Vec3& operator*=(const float f);
+    Vec3& operator/=(const float f);
+    Vec3& operator=(const Vec3& v);
+
+    void set(float vec_x, float vec_y, float vec_z);
+    void clear();
+};
+
+Vec3 operator+(const Vec3& v1, const Vec3& v2);
+Vec3 operator-(const Vec3& v1, const Vec3& v2);
+Vec3 operator*(const Vec3& v1, const float f);
+Vec3 operator*(const float f, const Vec3& v);
+
+class Matrix33{
+  public:
+    Vec3 v1, v2, v3;
+    Matrix33() : v1(), v2(), v3(){}
+    Matrix33(const Vec3 matrix_v1, const Vec3 matrix_v2, const Vec3 matrix_v3) : v1(matrix_v1), v2(matrix_v2), v3(matrix_v3){}
+    Matrix33(
+      float m00, float m01, float m02,
+      float m10, float m11, float m12,
+      float m20, float m21, float m22)
+    : v1(m00, m01, m02), v2(m10, m11, m12), v3(m20, m21, m22){}
+    
+    Vec3& operator[](int i){
+      return (i==0) ? v1 : (i==1) ? v2 : v3;
+    }
+    const Vec3& operator[](int i) const{
+      return (i==0) ? v1 : (i==1) ? v2 : v3;
+    }
+
+    Matrix33& operator+=(const Matrix33& matrix);
+    Matrix33& operator-=(const Matrix33& matrix);
+    Matrix33& operator*=(const Matrix33& matrix);
+    Matrix33& operator*=(const float f);
+
+    void set(Vec3 matrix_v1, Vec3 matrix_v2, Vec3 matrix_v3);
+    void clear();
+
+    static Matrix33 identity(){
+      return Matrix33(
+        1.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 1.0f);
+    }
+
+    static Matrix33 rotX(float deg){
+      double c = cos(deg / 180.0f * 3.14159265);
+      double s = sin(deg / 180.0f * 3.14159265);
+
+      return Matrix33(
+        1, 0, 0,
+        0, c, -s,
+        0, s, c);
+    }
+
+    static Matrix33 rotY(float deg){
+      double c = cos(deg / 180.0f * 3.14159265);
+      double s = sin(deg / 180.0f * 3.14159265);
+
+      return Matrix33(
+        c, 0, s,
+        0, 1, 0,
+        -s, 0, c);
+    }
+
+    static Matrix33 rotZ(float deg){
+      double c = cos(deg / 180.0f * 3.14159265);
+      double s = sin(deg / 180.0f * 3.14159265);
+
+      return Matrix33(
+        c, -s, 0,
+        s, c, 0,
+        0, 0, 1);
+    }
+};
+
+Matrix33 operator+(const Matrix33& m1, const Matrix33& m2);
+Matrix33 operator-(const Matrix33& m1, const Matrix33& m2);
+Matrix33 operator*(const Matrix33& m1, const Matrix33& m2);
+Matrix33 operator*(const Matrix33& m, const float f);
+Vec3 operator*(const Matrix33& m, const Vec3& v);
